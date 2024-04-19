328c8d63af058a779d165d65fdf5030b13ab3fd3
Emily Cheng <echeng0123@gmail.com>
2024-04-19 11:42:05 -0400

    setting up frontend architecture

---
+++ client/src/components/MainSection.jsx
@@ -0,0 +1,13 @@
+// This component handles routing for the entire app between the three "pages"
+
+import { Routes, Route } from "react-router-dom";
+
+export default function MainSection() {
+	return (
+		<section>
+			<Routes>
+				<Route></Route>
+			</Routes>
+		</section>
+	);
+}
