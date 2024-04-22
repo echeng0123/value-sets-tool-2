622ac712f195677c42f426c60706bf7c01ba614b
Emily Cheng <echeng0123@gmail.com>
2024-04-22 01:33:33 -0400

    connecting to internal db

--- server/db/client.js
+++ server/db/client.js
@@ -3,10 +3,10 @@
 const { Client } = require("pg");
 
 const database = "valuesets2";
-const client = new Client(`postgres://localhost:5432/${database}`);
-// const client = new Client(
-// 	`postgres://valuesets2_user:krkRCJQaNRob45zh94TvxWy31rarKqJJ@dpg-coiv125jm4es739v4ce0-a/valuesets2`
-// );
+// const client = new Client(`postgres://localhost:5432/${database}`);
+const client = new Client(
+	`postgres://valuesets2_user:krkRCJQaNRob45zh94TvxWy31rarKqJJ@dpg-coiv125jm4es739v4ce0-a/valuesets2`
+);
 
 // export for use in other files
 module.exports = client;
