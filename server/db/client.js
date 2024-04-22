11aae97b74f76cfd034fc6bffa051373b1ed574b
Emily Cheng <echeng0123@gmail.com>
2024-04-22 01:22:52 -0400

    pushinginternal database URL

--- server/db/client.js
+++ server/db/client.js
@@ -3,10 +3,10 @@
 const { Client } = require("pg");
 
 const database = "valuesets2";
-const client = new Client(`postgres://localhost:5432/${database}`);
-// const client = new Client(
-// 	`postgres://audiofile_user:YdUwC3IFfc2err9WFoatdE42STHUwhu4@dpg-cmrvn10cmk4c73840t4g-a/audiofile`
-// );
+// const client = new Client(`postgres://localhost:5432/${database}`);
+const client = new Client(
+	`postgres://valuesets2_user:krkRCJQaNRob45zh94TvxWy31rarKqJJ@dpg-coiv125jm4es739v4ce0-a/valuesets2`
+);
 
 // export for use in other files
 module.exports = client;
