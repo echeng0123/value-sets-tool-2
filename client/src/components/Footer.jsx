51410d53cd5ceb5a478bde39067a2a87beaa2537
Emily Cheng <echeng0123@gmail.com>
2024-04-22 00:34:31 -0400

    updated text in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,20 @@
 export default function Footer() {
 	return (
-		<section id="footer-container">
-			<h4 className="footer-item">Emily Cheng 2024</h4>
-			<button className="top-button">
-				<a href="#top">Back to Top</a>
-			</button>
-			<h4 className="footer-item">VCLIC Assessment</h4>
+		<section id="footer-container-big">
+			<div id="footer-container">
+				<h4 className="footer-item">Emily Cheng 2024</h4>
+				<button className="top-button">
+					<a href="#top">Back to Top</a>
+				</button>
+				<h4 className="footer-item">VCLIC Assessment</h4>
+			</div>
+			<div>
+				<p>This site is best viewed on desktop.</p>
+				<p>
+					Tech stack: React | Javascript | HTML/CSS | Node.js |
+					Express | PostgreSQL | Vite | Render | Material UI
+				</p>
+			</div>
 		</section>
 	);
 }
